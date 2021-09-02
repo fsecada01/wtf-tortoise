b83533e6dffbebb775d2cf2b676de1c972e8024d
Francis Secada <francis.secada@gmail.com>
2021-09-01 22:44:38 -0400

    Moved test file to tests subdirectory; hotfix for gitignore; removal of egg-info from code directory

--- setup.py
+++ setup.py
@@ -5,20 +5,22 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="wtf-tortoise",
-    version="0.0.1",
+    version="0.0.2",
     author="sinisaos (packaged by fsecada01",
     author_email="francis.secada@gmail.com",
     description="Implementation of WTForms for Tortoise ORM",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/fsecada01/wtf-tortoise",
-    project_url=None,
+    project_urls={
+        'Bug Tracker': "https://github.com/fsecada01/wtf-tortoise/issues",
+    },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     package_dir={"": "wtftortoise"},
-    packages=setuptools.find_packages(where=""),
+    packages=setuptools.find_packages(where="src"),
     python_requires=">=3.7",
 )
